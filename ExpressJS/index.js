05840b2ca6c3823273d4340b9f5da147d5677578
PranavAvasthi <pranavavasthi44@gmail.com>
2024-09-26 15:33:56 +0530

    post contact form data

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -3,10 +3,14 @@ import path from "path";
 
 const app = express();
 
+const users = [];
+
 // Setting up the View Engine
 app.set("view engine", "ejs");
 
+// Using Middlewares
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/getproducts", (req, res) => {
   // res.send("Hi");
@@ -29,6 +33,16 @@ app.get("/getproducts", (req, res) => {
 
 app.get("/", (req, res) => {
   res.render("index", { name: "Pranav" });
+});
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.post("/", (req, res) => {
+  users.push({ username: req.body.name, email: req.body.email });
+
+  res.redirect("/success");
 });
 
 app.listen(5000, () => {
